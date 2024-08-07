63c1150af3601e905a7d913934f3e1523cd55db5
Citrous06 <jewall009@yahoo.com>
2024-08-06 20:26:49 -0400

    test: added cases

--- tests/factories.py
+++ tests/factories.py
@@ -31,4 +31,10 @@ class ProductFactory(factory.Factory):
         model = Product
 
     id = factory.Sequence(lambda n: n)
+    name = FuzzyChoice(choices=["Hat" ,"chair" ,"Kia" ,"Chicken", "Pants", "Shirt", "Apple", "Banana", "Pots",  "Towels", "Ford",])
+    description = factory.Faker("text") 
+    price = FuzzyDecimal(0.5, 2000.0, 2)
+    available = FuzzyChoice(choices=[True,False])
+    category = FuzzyChoice(choices=[Category.UNKNOWN , Category.CLOTHS , Category.FOOD , Category.HOUSEWARES , Category.AUTOMOTIVE , Category.TOOLS])
+    
    ## Add code to create Fake Products 
